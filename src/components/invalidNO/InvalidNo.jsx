26839246872bdd62c678f9ddb00f393bcbbe1f05
Ahmed <atarek4852@gmail.com>
2025-01-30 13:36:54 +0200

    handle arabic error msg

--- src/components/invalidNO/InvalidNo.jsx
+++ src/components/invalidNO/InvalidNo.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import "./invalidNo.css"
+import React, { useContext } from "react";
+import "./invalidNo.css";
+import { LanguageContext } from "../../context/LanguageContext";
 
-export default function InvalidNo({lang}) {
+export default function InvalidNo({ lang }) {
+  const { language, changeLanguage } = useContext(LanguageContext);
+
   return (
-    <div className='d-flex flex-column align-items-center gap-3 my-5 py-5'>
-        <h1 className='heading'>NOT FOUND</h1>
-        <h3 className="c-text">Looks like you enterd an invalid tracking number</h3>
-        <h3 className="c-text">Please try valid tracking number</h3>
-      
+    <div className="d-flex flex-column align-items-center gap-3 my-5 py-5">
+      <h1 className="heading">{language == "English" ? "NOT FOUND" : "لم يتم العثور على شحنتك"}</h1>
+      <h3 className="c-text">
+        {language == "English"
+          ? "Looks like you enterd an invalid tracking number"
+          : "يبدو ان رقم التتبع غير صحيح"}
+      </h3>
+      <h3 className="c-text">
+        {language == "English" ? "Please try valid tracking number" : "برجاء إدخال رقم تتبع صحيح والمحاولة مره اخري"}
+      </h3>
     </div>
-  )
+  );
 }
